6570ef78dfc56f1110222b651e26787df16aaa23
ogaoga08 <kamekameshin0514@gmail.com>
2023-11-12 16:44:53 +0900

    カリキュラム08-6

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
     protected $fillable = [
         'title',
@@ -21,4 +23,3 @@ class Post extends Model
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
-
